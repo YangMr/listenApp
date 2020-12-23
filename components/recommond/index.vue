5110fb707d20fd73ac845b84c3b05ddb2520f64c
yangmr <759168442@qq.com>
2020-12-23 16:06:26 +0800

    修复了flex无法穿透组件的问题

--- components/recommond/index.vue
+++ components/recommond/index.vue
@@ -6,12 +6,7 @@
 		</my-list-header>
 		<view class="flex align-center ">
 			<block v-for="(item,index) in Rebooks" :key="index">
-				<view class="flex flex-column align-center flex-1 position-relative" style="height:300rpx;">
-					<image :src="item.src" mode="widthFix" class="w-100" style="height:100%"></image>
-					<text class="font my-1 text-light-black">{{item.name}}</text>
-					<my-icon iconColor="text-light-white" iconName="icon-erji" class="position-absolute" iconSize="35" style=" left :45rpx ; top: 20rpx;"></my-icon>
-				</view>
-				<!-- <my-item :itemSrc="item.src" :itemName="item.name"></my-item> -->
+				<my-item :itemSrc="item.src" :itemName="item.name"></my-item>
 			</block>
 		</view>
 	</view>
--- components/recommond/item.vue
+++ components/recommond/item.vue
@@ -1,6 +1,6 @@
 <template>
-	<view class="flex flex-column align-center position-relative bg-secondary" style="height:300rpx;flex:1;">
-		<image :src="itemSrc" mode="widthFix" class="w-100" style="height:100%"></image>
+	<view class="flex flex-column align-center flex-1 position-relative" style="height:300rpx;overflow:hidden">
+		<image :src="itemSrc" mode="widthFix" style="height:100%; width: 250rpx;"></image>
 		<text class="font my-1 text-light-black">{{itemName}}</text>
 		<my-icon iconColor="text-light-white" iconName="icon-erji" class="position-absolute" iconSize="35" style=" left :45rpx ; top: 20rpx;"></my-icon>
 	</view>
